dd76551e31bf3c269ddaee3655d2c555860182b6
bigudam <dharma.herrera@gmail.com>
2019-04-03 09:32:27 -0300

    estructura de htmal mas avances en js

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,43 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
-  },
+  encode: (mensaje, offset) => {
+    let msnCifrado= nuevaLetra;
+    for(let i=0; i<=mensaje.legnth; i++){
+    let numeroAscii=mensaje.charCodeAt(0);
+    if(numeroAscii >=65 && numeroAscii<=90){
+    let formula= (numeroAscii-65+parseInt(offset))&26+65;
+    let nuevaLetra= string.fromCharCode(formula);
+    console.log(msnCifrado)
+    }
+    }
+
+
+
+/*for (let i = 0; i < mensaje.length; i++) {
+  let letraAscii = mensaje[i].charCodeAt(0);
+  console.log(letraAscii)
+  if (letraAscii >= 65 && letraAscii <= 90){
+    let nuevaLetraAscii= (letraAscii-65+offset)%26+65;
+    let nuevaLetra = nuevaLetraAscii.String.fromCharCode(nuevaLetraAscii);
+  }
+  console.log(nuevaLetra)
+}*/
+}
+
+
   decode: () => {
-    /* Acá va tu código */
+    let msnCifrado= nuevaLetra;
+    for(let i=0; i<=mensaje.legnth; i++){
+    let numeroAscii=mensaje.charCodeAt(0);
+    if(numeroAscii >=65 && numeroAscii<=90){
+    let formula= (numeroAscii+ 65-parseInt(offset))&26+65;
+    let nuevaLetra= string.fromCharCode(formula);
+    console.log(msnCifrado)
   }
 };
+
+//FORMULA: (x+n)%26
+//de letra a ascii charCodeAt
+//de ascii a letra fromChardCode
+//iteración (i)es ir de uno en uno
+// 65 a 90 mayúsculas
+//ciclo es...(de letra a ascii)-(ascii a formula)-(nuevo ascii a letra)
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+let mensaje= document.getElementsByTagName('imput')[0]
+let espacios = document.getElementByName('input')[1]
+
+document.getElementById("cifrar").addEventListener("click", () => {
+}
+document.getElementById("descifrar").addEventListener("click", () => {
+}
